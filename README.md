830f7aa0014d589cc71e191a7dbaedb2ecbd3086
“dengs” <1019217919@qq.com>
2020-12-15 22:54:49 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,153 @@
+# 部署文档
+
+
+
+## 架构说明
+
+Capture 部署到网关服务器，或者web服务器，进行访问流量抓取
+
+CopyServer部署到一台服务器作为复制节点，会接收Capture 抓取的流量
+
+Sender部署到一台服务器作为流量发送节点，放大流量倍数发送到测试环境或生产环境 
+
+Senderpool是sender的升级版，支持定时、并发发送峰值流量
+
+
+
+## 部署步骤
+
+**以Centos7为例, 语言版本python2**
+
+### 安装系统依赖
+
+```bash
+yum -y install flex.x86_64
+yum -y install bison.x86_64
+yum -y install byacc.x86_64
+yum -y install python-devel.x86_64
+yum -y install gcc unzip
+```
+
+### 安装lib依赖库
+
+```bash
+cd Meteor/lib
+tar -xvf libpcap-1.7.4.tar.gz
+cd libpcap-1.7.4/
+./configure && make install
+unzip pypcap-master.zip
+cd pypcap-master/
+python setup.py install
+unzip dpkt-master.zip
+cd dpkt-master/
+python setup.py install
+
+ln -s /usr/local/lib/libpcap.so.1  /usr/lib/
+```
+
+
+
+### 启动capture
+
+#### 节点ip
+
+192.168.1.1
+
+#### 配置文件
+
+```bash
+[root@k8s-node2 capture]# cat capture.conf 
+[filter]
+# 过滤参数支持uri 和端口过滤
+uri=
+host=
+[config]
+# capture节点网卡
+nc=ens33
+# capture节点抓取端口
+capture_port=80
+# copyserver节点ip地址
+copy_ip=192.168.1.2
+# 端口默认65533
+copy_port=65533
+model=
+[exclude]
+# 不抓取的源ip
+ip=
+
+
+```
+
+#### 启动命令
+
+```bash
+python start_capture.py
+```
+
+
+
+### 启动copyserve
+
+##### 节点ip
+
+192.168.1.2
+
+##### 配置文件
+
+```bash
+[root@k8s-node2 copyserver]# cat copy.conf 
+[filter]
+# 支持url过滤参数
+uri=
+[config]
+# 指定网卡
+nc=ens33
+# 端口默认65533
+port=
+# 默认使用本机IP地址发送udp广播，支持配置多个sendserver接收ip地址，为空就广播发送，不为空就是发送给指定ip
+ip=
+
+```
+
+##### 启动命令
+
+```bash
+python start_copy.py
+```
+
+
+
+### 启动sendserve
+
+##### 节点ip
+
+192.168.1.3 （同一网段即可，只要能接收udp广播流量， 可以启动多个）
+
+##### 配置文件
+
+```bash
+[root@k8s-node1 sender]# cat send.conf 
+[filter]
+# 过滤参数
+uri=
+[config]
+# 网卡
+nc=ens33
+# 修改访问ip，这里配置测试环境地址即可把流量发送到测试环境
+resent_ip=192.168.1.11
+# 放大流量倍数， 默认5
+count=5
+# 发送端口， 默认65533
+port=
+
+```
+
+##### 启动命令
+
+```bash
+python start_sender.py
+```
+
+
+
+**ps:流量不存储，直接转发**
---
+++ lib/pypcap-master/setup.py
@@ -0,0 +1,113 @@
+"""Setup for pycapy"""
+from setuptools import setup, Extension
+import glob
+import os
+import sys
+import re
+
+def recursive_search_dirs(dirs, target_files):
+    """Recursive search directories"""
+    for d in dirs:
+        r = recursive_search(d, target_files)
+        if r:
+            return r
+
+def recursive_search(path, target_files):
+    """Recursively search for files"""
+    for root, _dirs, files in os.walk(path):
+        for filename in files:
+            if filename in target_files:
+                return os.path.join(root, filename)
+
+# A list of all the possible search directories
+dirs = ['/usr', sys.prefix] + glob.glob('/opt/libpcap*') + \
+    glob.glob('../libpcap*') + glob.glob('../wpdpack*') + \
+    ['/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/'] + \
+     glob.glob('/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*')
+
+for d in dirs:
+    # This makes sure that we first search inside of */include/pcap
+    search_dirs = [os.path.join(d, 'usr', 'include', 'pcap'),
+                   os.path.join(d, 'include', 'pcap'),
+                   os.path.join(d, 'local', 'include', 'pcap'),
+                   d]
+
+    pcap_h = recursive_search_dirs(search_dirs, ['pcap.h'])
+    if pcap_h:
+        print "Found pcap headers in %s" % pcap_h
+        break
+
+if not pcap_h:
+    print "pcap.h not found"
+    sys.exit(1)
+
+include_dirs = [os.path.dirname(pcap_h)]
+# FIXME: This is super weird 'd' here is what? the last d in dir list?
+lib_sub_dirs = [os.path.join(d, sub_dir) \
+        for sub_dir in ('lib', 'lib64', \
+        'lib/x86_64-linux-gnu', 'lib/i386-linux-gnu', '')]
+
+lib_files = [
+    'libpcap.a',
+    'libpcap.so',
+    'libpcap.dylib',
+    'wpcap.lib'
+]
+lib_file_path = recursive_search_dirs(lib_sub_dirs, lib_files)
+
+print "Found libraries in %s" % lib_file_path
+
+lib_file = os.path.basename(lib_file_path)
+
+extra_compile_args = []
+if re.match(r"libpcap\.(a|so|dylib)", lib_file):
+    libraries = ('pcap',)
+elif lib_file == "wpcap.lib":
+    libraries = ('wpcap', 'iphlpapi')
+    extra_compile_args = ['-DWIN32', '-DWPCAP']
+
+define_macros = []
+
+pcap_int_path = recursive_search_dirs(dirs, ['pcap-int.h'])
+if pcap_int_path:
+    pcap_int_dir = os.path.dirname(pcap_int_path)
+    if pcap_int_dir not in include_dirs:
+        include_dirs.append(pcap_int_dir)
+    define_macros.append(('HAVE_PCAP_INT_H', 1))
+else:
+    print "No pcap-int.h found"
+
+pcap_h_file = open(pcap_h).readlines()
+for line in pcap_h_file:
+    if 'pcap_file(' in line:
+        print "found pcap_file function"
+        define_macros.append(('HAVE_PCAP_FILE', 1))
+    if 'pcap_compile_nopcap(' in line:
+        print "found pcap_compile_nopcap function"
+        define_macros.append(('HAVE_PCAP_COMPILE_NOPCAP', 1))
+    if 'pcap_setnonblock(' in line:
+        print "found pcap_setnonblock"
+        define_macros.append(('HAVE_PCAP_SETNONBLOCK', 1))
+    if 'pcap_setdirection(' in line:
+        print "found pcap_setdirection"
+        define_macros.append(('HAVE_PCAP_SETDIRECTION', 1))
+
+
+pcap = Extension(
+    name='pcap',
+    sources=['pcap.c', 'pcap_ex.c'],
+    include_dirs=include_dirs,
+    define_macros=define_macros,
+    libraries=list(libraries),
+    extra_compile_args=extra_compile_args,
+)
+
+setup(
+    name='pypcap',
+    version='1.1.4',
+    author='Dug Song',
+    author_email='dugsong@monkey.org',
+    url='http://monkey.org/~dugsong/pypcap/',
+    description='packet capture library',
+    ext_modules=[pcap],
+)
